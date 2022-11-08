42f54dc4f8adbc189d5ace2218dea22c2768584b
Espen Holm Pedersen <91594315+PederZzen@users.noreply.github.com>
2022-11-08 15:39:53 +0100

    installed html loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,22 @@
+const htmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
   entry: './src/js/index.js',
   output: {
     filename: './js/main.js',
+  },
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+            options: { minimize: true },
+          },
+        ],
+      },
+    ],
   },
 };
