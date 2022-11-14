348721490aaf12d6e629c0c13caee7561ef465b1
Espen Holm Pedersen <91594315+PederZzen@users.noreply.github.com>
2022-11-14 11:15:48 +0100

    e2e testing complete..maybe

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -2,22 +2,43 @@ import { email } from './user.js';
 import { password } from './user.js';
 
 describe('Test of login validations', () => {
-    it('Tests if the login form validates user inputs correctly based on API restrictions', () => {
-        cy.visit('/');
-        cy.get('#registerForm button')
+    it('Tests that the login button does not do anything without form-input', () => {
+        cy.goToLoginForm();
+        cy.get('#loginForm button')
             .contains('Login')
-            .wait(500)
-            .click()
-            .wait(500);
-        cy.get('#loginEmail')
-            .type('superman@noroff.no');
-        cy.get('#loginPassword')
-            .type(password);
+            .click();
+        cy.url().should('not.include', '?view=profile');
+    });
+
+    it('Tests for invalid email', () => {
+        cy.goToLoginForm();
+        cy.get('#loginEmail').type('superman@noroff.no');
+        cy.get('#loginPassword').type(password);
         cy.get('#loginForm button')
             .contains('Login')
             .click();
         cy.getLocalStorage('token').then((token) => {
             expect(token).to.be.null;
+        });        
+    });
+
+    it('Tests for invalid password', () => {
+        cy.goToLoginForm();
+        cy.get('#loginEmail').type(email);
+        cy.get('#loginPassword').type('2short');
+        cy.get('#loginForm button')
+        .contains('Login')
+        .click();
+        cy.getLocalStorage('token').then((token) => {
+            expect(token).to.be.null;
+        }); 
+    });
+
+    it('Tests for valid detalis', () => {
+        cy.goToLoginForm();
+        cy.typeLoginDetails();
+        cy.getLocalStorage('token').then((token) => {
+            expect(token).to.not.be.null;
         });        
     });
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -3,6 +3,9 @@ import { logout } from '../../src/js/api/index';
 import { createPost } from '../../src/js/api/index';
 import { deletePost } from '../../src/js/api/index';
 
+import { email } from '../e2e/user.js';
+import { password } from '../e2e/user.js';
+
 Cypress.Commands.add('login', async (email, password) => {
   await login(email, password);
 });
@@ -18,4 +21,22 @@ Cypress.Commands.add('createItem', async (title, body, media, tags) => {
 
 Cypress.Commands.add('deletePost', (id) => {
   deletePost(id);
+});
+
+Cypress.Commands.add('goToLoginForm', () => {
+  cy.visit('/');
+  cy.get('#registerForm button')
+      .contains('Login')
+      .wait(500)
+      .click()
+      .wait(500);
+});
+
+Cypress.Commands.add('typeLoginDetails', () => {
+  cy.get('#loginEmail').type(email);
+  cy.get('#loginPassword').type(password);
+  cy.get('#loginForm button')
+      .contains('Login')
+      .click()
+      .wait(500);
 });
